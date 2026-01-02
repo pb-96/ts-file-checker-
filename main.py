c823e58d842422aa2432935f79502f8ded37f6ae
pb-96 <philip.byrne@faculty.ai>
2026-01-02 17:37:34 +0000

    adding updated find and replace

--- main.py
+++ main.py
@@ -4,37 +4,60 @@ from collections import deque
 import os
 
 
+# TODO: Figure out how to make this configure able
+IGNORE_FOLDERS = {
+    "node_modules",
+
+}
+
 def validate_path(given_path: Path) -> bool:
-    if not given_path.exists():
-        return False
-
-    return True
+    return given_path.exists()
 
 
-def convert_to_camel(file_name: Path, file_type: str) -> str:
-    if not "_" in file_name:
-        return file_name
+def convert_to_camel(file_name: Path, file_type: str) -> Path:
+    if "_" in file_name.name:
+        as_list = file_name.name.replace(file_type, "").lower().split("_")
 
-    as_list = file_name.name.replace(file_type, "").lower().split("_")
-    it = iter(as_list)
+        converted_file_name = as_list[0]
+        for elem in as_list[1:]:
+            converted_file_name = converted_file_name + elem.title()
 
-    converted_file_name = next(it)
-    for elem in as_list:
-        converted_file_name = converted_file_name + elem.title()
+        return file_name.parent.resolve() / f"{converted_file_name}{file_type}"
 
-    return f"{converted_file_name}{file_type}"
+    return file_name
 
+
+def handle_file(file_path: Path, file_type: str):
+    adjusted_path = convert_to_camel(file_path, file_type)
+    if adjusted_path == file_path:
+        return
+
+    os.rename(str(file_path.resolve()), str(adjusted_path.resolve()))
+
+
+def predicate_file(file_path: Path, file_type: str):
+    return (
+        file_path.is_file()
+        and file_path.name.endswith(file_type)
+        and validate_path(file_path)
+    )
+
+def validate_folder(_path: Path):
+    return _path.is_dir() and not _path.name in IGNORE_FOLDERS
 
 def traverse_target(given_path: Path, file_type: str):
     to_search = deque([given_path])
     while to_search:
         this_layer = to_search.pop()
-        for file_path in this_layer.iterdir():
-            if file_path.is_file() and file_path.name.endswith(file_type):
-                adjusted_path = this_layer / convert_to_camel(file_path)
-                os.rename(str(file_path.resolve()), str(adjusted_path.resolve()))
-            else:
-                to_search.append(file_path)
+        if validate_folder(this_layer):
+            for file_path in this_layer.iterdir():
+                if predicate_file(file_path, file_type):
+                    handle_file(file_path, file_type)
+                else:
+                    to_search.append(file_path)
+
+        if predicate_file(file_path, file_type):
+            handle_file(file_path, file_type)
 
 
 def main(given_path: Path, file_type: str = ".ts"):
